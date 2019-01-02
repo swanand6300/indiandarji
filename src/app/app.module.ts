48a497facbf4a3552da7e99d6b33c096a3d15881
Swanand Taware <swanand.taware@enyota.com>
2019-01-02 18:15:41 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,45 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { NgModule, ErrorHandler } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage } from "../pages/about/about";
+import { ContactPage } from "../pages/contact/contact";
+import { HomePage } from "../pages/home/home";
+import { TabsPage } from "../pages/tabs/tabs";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { AngularFireModule } from 'angularfire2';
+// for AngularFireDatabase
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+// for AngularFireAuth
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { CallNumber } from '@ionic-native/call-number';
+export const firebase  = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule 
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
+  entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    CallNumber,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule {
+  
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,70 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { AngularFireDatabase } from "angularfire2/database";
+import { ToastController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import { CallNumber } from '@ionic-native/call-number';
+import { ActionSheetController } from 'ionic-angular';
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  customer;
+  itemsRef;
+  loader = this.loadingCtrl.create({
+    content: "Please wait..."
+  });
+  callNo;
+  constructor(public navCtrl: NavController, db: AngularFireDatabase,public toastCtrl: ToastController,public loadingCtrl: LoadingController,private callNumber: CallNumber,public actionSheetCtrl: ActionSheetController) {
+    this.loader.present();
+    this.itemsRef = db.list("/darji").snapshotChanges()
+    this.itemsRef.subscribe(items => {
+      this.customer = [items][0];
+      this.customer = this.customer.map(action =>  ({ key: action.key, ...action.payload.val() }));
+      this.loader.dismiss();
+      this._fnRecordFetchToast("Dashboard updated!");
+    });
+    
   }
-
+  _fncallNumber(number){
+    this.callNumber.callNumber(number, true)
+  .then(res => console.log('Launched dialer!', res))
+  .catch(err => console.log('Error launching dialer', err));
+  }
+  
+  _fnRecordFetchToast(val) {
+    const toast = this.toastCtrl.create({
+      message: val,
+      duration: 3000
+    });
+    toast.present();
+  }
+  _fnpresentActionSheet(person) {
+    var key = person.key;
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify customer details',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            this._fnRecordFetchToast('Destructive '+key);
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            this._fnRecordFetchToast('Archive '+key);
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this._fnRecordFetchToast('Cancel '+key);
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 }
